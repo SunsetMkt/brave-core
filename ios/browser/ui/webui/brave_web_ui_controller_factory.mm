57c3b8992e609a4066e6a66808f0b41588d1ca4d
Stephen Heaps <steaps.4@gmail.com>
2024-06-21 17:44:29 -0400

    Add UntrustedNftUI for nft-display iframes on iOS.

--- ios/browser/ui/webui/brave_web_ui_controller_factory.mm
+++ ios/browser/ui/webui/brave_web_ui_controller_factory.mm
@@ -15,6 +15,7 @@
 #include "brave/components/constants/webui_url_constants.h"
 #include "brave/ios/browser/ui/webui/skus/skus_internals_ui.h"
 #include "brave/ios/browser/ui/webui/wallet/brave_wallet_page_ui.h"
+#include "brave/ios/browser/ui/webui/wallet/nft_ui.h"
 #include "build/build_config.h"
 #include "components/prefs/pref_service.h"
 #include "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
@@ -67,6 +68,8 @@ WebUIIOSFactoryFunction GetWebUIIOSFactoryFunction(const GURL& url) {
     return &NewWebUIIOS<SkusInternalsUI>;
   } else if (url_host == kWalletPageHost) {
     return &NewWebUIIOS<BraveWalletPageUI>;
+  } else if (url_host == kUntrustedNftHost) {
+    return &NewWebUIIOS<UntrustedNftUI>;
   }
   return nullptr;
 }
